b3577ec0230bfbfd785d13cd6762b0c46e535f51
tina067 <tinamurmu222@gmail.com>
2024-06-14 13:33:01 +0530

    added all the features

--- components/common/Container.js
+++ components/common/Container.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useWindowWidth } from '../hooks/useWindowWidth';
 
 export default function Container({ children }) {
@@ -14,6 +15,31 @@ export default function Container({ children }) {
       }}
     >
       <div style={{ width: isSmallerDevice ? '95%' : '85%' }}>{children}</div>
- </div>
-);
+    </div>
+  );
 }
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { useWindowWidth } from '../hooks/useWindowWidth';
+
+// export default function Container({ children }) {
+//   const { isSmallerDevice } = useWindowWidth();
+
+//   return (
+//     <div
+//       style={{
+//         display: 'flex',
+//         flexDirection: 'column',
+//         width: '100%',
+//         alignItems: 'center',
+//       }}
+//     >
+//       <div style={{ width: isSmallerDevice ? '95%' : '85%' }}>{children}</div>
+//  </div>
+// );
+// }
--- components/hooks/useWindowWidth.js
+++ components/hooks/useWindowWidth.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const WindowWidthContext = createContext();
 
@@ -26,4 +27,39 @@ export const WindowWidthProvider = ({ children }) => {
   );
 };
 
+WindowWidthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 export const useWindowWidth = () => useContext(WindowWidthContext);
+
+
+// import React, { createContext, useContext, useEffect, useState } from 'react';
+
+// const WindowWidthContext = createContext();
+
+// export const WindowWidthProvider = ({ children }) => {
+//   const [isSmallerDevice, setIsSmallerDevice] = useState(false);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       const width = window.innerWidth;
+//       setIsSmallerDevice(width < 500);
+//     };
+
+//     handleResize();
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   return (
+//     <WindowWidthContext.Provider value={{ isSmallerDevice }}>
+//       {children}
+//     </WindowWidthContext.Provider>
+//   );
+// };
+
+// export const useWindowWidth = () => useContext(WindowWidthContext);
